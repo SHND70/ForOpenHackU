82989c665c7a3499479b24825d8666e11a6552ff
SHND70 <n.shindoh.1999@gmail.com>
2021-09-03 22:28:59 +0900

    first

---
+++ Script/ScreenShot.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class ScreenShot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string printName;
+    public GameObject gameobject;
+    private SpriteRenderer sr;
+
+    public Text imageName;
+
+    public void Update()
+    {
+        printName = imageName.text;
+    }
+
+    public void OnClick() {
+        sr = gameobject.GetComponent<SpriteRenderer>();
+        StartCoroutine(Capture((int)sr.sprite.texture.width,(int)sr.sprite.texture.height));
+    }
+
+    public virtual IEnumerator Capture(int width,int height)
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+        int x = (tex.width - width)/2;
+        int y = (tex.height - height)/2;
+        Color[] colors = tex.GetPixels(x,y,width,height);
+        Texture2D saveTex = new Texture2D(width,height,TextureFormat.ARGB32,false);
+        saveTex.SetPixels(colors);
+        File.WriteAllBytes("Assets/ScreenShots/"+printName + ".png",saveTex.EncodeToPNG());
+        yield break;
+    }
+}
