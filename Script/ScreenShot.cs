40caa493412f65a7a3ab6c5c68e7c02e4ed8a962
SHND70 <57438244+SHND70@users.noreply.github.com>
2021-09-07 06:39:03 +0900

    Data Add

--- Script/ScreenShot.cs
+++ Script/ScreenShot.cs
@@ -16,12 +16,16 @@ public class ScreenShot : MonoBehaviour
     public int width;
     public int height;
 
+    public int[,][] Data;
+    public int[][] Col;
+
     public void Update()
     {
         printName = imageName.text;
     }
 
     public void OnClick() {
+        Data = new int[width,height][];
        StartCoroutine(Capture(width,height));
     }
 
@@ -35,6 +39,22 @@ public class ScreenShot : MonoBehaviour
         Color[] colors = tex.GetPixels(x,y,width,height);
         Texture2D saveTex = new Texture2D(width,height,TextureFormat.ARGB32,false);
         saveTex.SetPixels(colors);
+
+        Col = new int[colors.Length][];
+        for(int i = 0;i<colors.Length;i++){
+            Col[i] = new int[]{(int)(colors[i].r*255),(int)(colors[i].g*255),(int)(colors[i].b*255),(int)(colors[i].a*255)};
+            //Debug.Log(Col[i][0]+","+Col[i][1]+","+Col[i][2]+","+Col[i][3]);
+        }
+
+        int C = 0;
+        for(int w=0;w<width;w++){
+            for(int h = 0;h<height;h++){
+                Data[w,h] = Col[C];
+                Debug.Log(Data[w,h][0]+","+Data[w,h][1]+","+Data[w,h][2]+","+Data[w,h][3]);
+                C++;
+            }
+        }
+
         File.WriteAllBytes("Assets/ScreenShots/"+printName + ".png",saveTex.EncodeToPNG());
         yield break;
     }
